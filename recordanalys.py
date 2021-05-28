c06e6d1b7bbcdc0e3ce350705a43a5d1eaba7815
agesz99 <a.akos.99@gmail.com>
2021-05-28 13:03:08 +0200

    loop entire prog and input to see plots

--- recordanalys.py
+++ recordanalys.py
@@ -7,134 +7,150 @@ import matplotlib.pyplot as plt
 import numpy as np
 import shutil
 
-print("...WELCOME TO PYTHON DSP...")
-rt = int(input("Enter recording time:"))
-name = input("The name of record:")
-print("recording time is: " + str(rt))
+while True:
 
-#create dir for analys figures
-dir_data = name + '_data'
-dir_sound = 'records'
-try:
-    os.mkdir(dir_data)
-    os.mkdir(dir_sound)
-    print("Directory " , dir_data ,  " Created!")
-    print("Directory"  , dir_sound , "Created!") 
-except FileExistsError:
-    print("Directory " , dir_data ,  " already exists!")
-    print("Directory"  , dir_sound , "already exist!")
+    print("...WELCOME TO PYTHON DSP...")
+    rt = int(input("Enter recording time:"))
+    name = input("The name of record:")
+    print("recording time is: " + str(rt))
 
-#route dir
-script_dir = os.path.dirname(__file__)
-results_dir = os.path.join(script_dir, dir_data + '/')
+    #create dir for analys figures
+    dir_data = name + '_data'
+    dir_sound = 'records'
+    try:
+        os.mkdir(dir_data)
+        os.mkdir(dir_sound)
+        print("Directory " , dir_data ,  " Created!")
+        print("Directory"  , dir_sound , "Created!") 
+    except FileExistsError:
+        print("Directory " , dir_data ,  " already exists!")
+        print("Directory"  , dir_sound , "already exist!")
+
+    #route dir
+    script_dir = os.path.dirname(__file__)
+    results_dir = os.path.join(script_dir, dir_data + '/')
 
 
-chunk = 1024  # Record in chunks of 1024 samples
-sample_format = pyaudio.paInt16  # 16 bits per sample
-channels = 1
-fs = 44100  # Record at 44100 samples per second
-seconds = rt
-filename = name + ".wav"
+    chunk = 1024  # Record in chunks of 1024 samples
+    sample_format = pyaudio.paInt16  # 16 bits per sample
+    channels = 1
+    fs = 44100  # Record at 44100 samples per second
+    seconds = rt
+    filename = name + ".wav"
 
-p = pyaudio.PyAudio()  # Create an interface to PortAudio
+    p = pyaudio.PyAudio()  # Create an interface to PortAudio
 
-print('Recording')
+    print('Recording')
 
-stream = p.open(format=sample_format,
+    stream = p.open(format=sample_format,
                 channels=channels,
                 rate=fs,
                 frames_per_buffer=chunk,
                 input=True)
 
-frames = []  # Initialize array to store frames
+    frames = []  # Initialize array to store frames
 
-# Store data in chunks 
-for i in range(0, int(fs / chunk * seconds)):
-    data = stream.read(chunk)
-    frames.append(data)
+    # Store data in chunks 
+    for i in range(0, int(fs / chunk * seconds)):
+        data = stream.read(chunk)
+        frames.append(data)
 
-# Stop and close the stream 
-stream.stop_stream()
-stream.close()
-# Terminate the PortAudio interface
-p.terminate()
+    # Stop and close the stream 
+    stream.stop_stream()
+    stream.close()
+    # Terminate the PortAudio interface
+    p.terminate()
 
-print('Finished recording. Start analysis')
+    print('Finished recording. Start analysis')
 
-# Save the recorded data as a WAV file
-wf = wave.open(filename, 'wb')
-wf.setnchannels(channels)
-wf.setsampwidth(p.get_sample_size(sample_format))
-wf.setframerate(fs)
-wf.writeframes(b''.join(frames))
-wf.close()
-
-
-
-FIG_SIZE = (3,4)
-
-sound = name + ".wav"
-
-#plot waveform sr = sample rate
-sound, sr = librosa.load(sound, sr = 22050)
-
-#waveform
-plt.figure(figsize=FIG_SIZE)
-librosa.display.waveplot(sound, sr = sr, alpha=0.4)
-plt.xlabel("time (s)")
-plt.ylabel("amplitude")
-plt.title(name + "  Waveform", fontweight='bold')
-#save waveform to _data dir
-sample_file_name = "waveform"
-plt.savefig(results_dir + sample_file_name)
-
-#spectogram
-n_fft = 2048
-hop_length = 512
-
-#print("STFT hop length duration is: {}s".format(hop_length_duration))
-#print("STFT window duration is: {}s".format(n_fft_duration))
-
-stft = librosa.core.stft(sound, hop_length=hop_length, n_fft=n_fft)
-spectogram = np.abs(stft)
-log_spectrogram = librosa.amplitude_to_db(spectogram)
-
-plt.figure(figsize=FIG_SIZE)
-librosa.display.specshow(log_spectrogram, sr=sr, hop_length=hop_length)
-plt.xlabel("Time")
-plt.ylabel("Frequency")
-plt.colorbar(format="%+2.0f dB")
-plt.title(name + "  Spectrogram (dB)", fontweight='bold')
-sample_file_name = "spectogram"
-plt.savefig(results_dir + sample_file_name)
+    # Save the recorded data as a WAV file
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(sample_format))
+    wf.setframerate(fs)
+    wf.writeframes(b''.join(frames))
+    wf.close()
 
 
-#spectrum
-plt.figure(figsize=FIG_SIZE)
-fft_spectrum = np.fft.rfft(sound)
-freq = np.fft.rfftfreq(sound.size, d=1./sr)
-fft_spectrum_abs = np.abs(fft_spectrum)
-plt.plot(freq[:4000], fft_spectrum_abs[:4000])
-plt.plot(freq, fft_spectrum_abs)
-plt.xlabel("frequency, Hz")
-plt.ylabel("Amplitude, units")
-plt.title(name + "  Frequency Domain", fontweight='bold')
-plt.xscale("log")
-plt.grid()
-sample_file_name = "spectrum"
-plt.savefig(results_dir + sample_file_name)
+
+    FIG_SIZE = (3,4)
+
+    sound = name + ".wav"
+
+    #plot waveform sr = sample rate
+    sound, sr = librosa.load(sound, sr = 22050)
+
+    #waveform
+    plt.figure(figsize=FIG_SIZE)
+    librosa.display.waveplot(sound, sr = sr, alpha=0.4)
+    plt.xlabel("time (s)")
+    plt.ylabel("amplitude")
+    plt.title(name + "  Waveform", fontweight='bold')
+    #save waveform to _data dir
+    sample_file_name = "waveform"
+    plt.savefig(results_dir + sample_file_name)
+
+    #spectogram
+    n_fft = 2048
+    hop_length = 512
+
+    #print("STFT hop length duration is: {}s".format(hop_length_duration))
+    #print("STFT window duration is: {}s".format(n_fft_duration))
+
+    stft = librosa.core.stft(sound, hop_length=hop_length, n_fft=n_fft)
+    spectogram = np.abs(stft)
+    log_spectrogram = librosa.amplitude_to_db(spectogram)
+
+    plt.figure(figsize=FIG_SIZE)
+    librosa.display.specshow(log_spectrogram, sr=sr, hop_length=hop_length)
+    plt.xlabel("Time")
+    plt.ylabel("Frequency")
+    plt.colorbar(format="%+2.0f dB")
+    plt.title(name + "  Spectrogram (dB)", fontweight='bold')
+    sample_file_name = "spectogram"
+    plt.savefig(results_dir + sample_file_name)
 
 
-# show plots
-plt.show()
+    #spectrum
+    plt.figure(figsize=FIG_SIZE)
+    fft_spectrum = np.fft.rfft(sound)
+    freq = np.fft.rfftfreq(sound.size, d=1./sr)
+    fft_spectrum_abs = np.abs(fft_spectrum)
+    plt.plot(freq[:4000], fft_spectrum_abs[:4000])
+    plt.plot(freq, fft_spectrum_abs)
+    plt.xlabel("frequency, Hz")
+    plt.ylabel("Amplitude, units")
+    plt.title(name + "  Frequency Domain", fontweight='bold')
+    plt.xscale("log")
+    plt.grid()
+    sample_file_name = "spectrum"
+    plt.savefig(results_dir + sample_file_name)
 
-#plot storage
-choice1 = input('Do you want saving data?(y/n)')
-if choice1 == 'n':
-    del_path = results_dir
-    shutil.rmtree(del_path, ignore_errors=True)
-    print('Data deleted.')
-elif choice1 == 'y':
-    print("Data saved!")
-else:
-    print('This is not a choice.')
+
+    # show plots
+    choice3 = input("Do you want to see the plots?(y/n)")
+    if 'y' in choice3:
+        plt.show()
+    elif 'n' in choice3:
+        pass
+    else:
+        print('invalid input')
+
+    #plot storage
+    choice1 = input('Do you want saving data?(y/n)')
+    if choice1 == 'n':
+        del_path = results_dir
+        shutil.rmtree(del_path, ignore_errors=True)
+        print('Data deleted.')
+    elif choice1 == 'y':
+        print("Data saved!")
+    else:
+        print('This is not a choice.')
+    
+
+    choice2 = input("Run again?(y/something) ")
+    if 'y' in choice2:
+            continue
+    else:
+            print("Good Bye")
+            break
